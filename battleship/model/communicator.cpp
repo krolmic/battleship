49839d79b0252374c06fcda6537a77ad88de5077
coffeemaker2017 <info@michael-krol.de>
2017-07-03 22:18:45 +0200

    Communicator: grundlegende Funktionen

--- battleship/model/communicator.cpp
+++ battleship/model/communicator.cpp
@@ -10,7 +10,7 @@ Communicator::~Communicator()
 {
 }
 
-void Communicator::encodeJson(T obj, QString& str)
+void Communicator::encodePlayerToJsonString(Player obj, QString& str)
 {
     QJsonObject object;
     obj.write(object);
@@ -19,7 +19,37 @@ void Communicator::encodeJson(T obj, QString& str)
     str = data;
 }
 
-void MODEL::Communicator::decodeJson(QString str, T& obj)
+void Communicator::decodeJsonStringToPlayer(QString str, Player& obj)
 {
+    QJsonObject obj = createJsonObjectFromString(str);
+    obj.read(obj);
+}
 
+QJsonObject Communicator::createJsonObjectFromString(const QString& in)
+{
+    QJsonObject obj;
+    QJsonDocument doc = QJsonDocument::fromJson(in.toUtf8());
+    // hat es geklappt?
+    if(!doc.isNull())
+    {
+        if(doc.isObject())
+            obj = doc.object();
+        else
+            qDebug() << "QJsonDocument enthaelt mehrere Objekte" << endl;
+    }
+    else
+        qDebug() << "JSON-Format nicht erkannt" << in << endl;
+    return obj;
+}
+
+void Communicator::sendData()
+{
+    // encode...
+    // connection->send(encodedData)
+}
+
+void Communicator:recieveData()
+{
+    // decode...
+    // switch-case
 }
--- battleship/model/communicator.h
+++ battleship/model/communicator.h
@@ -4,31 +4,42 @@
 #include <QJsonObject>
 #include <QJsonDocument>
 #include "connection.h"
+#include "gui/player.h"
 
 namespace MODEL {
-
-template <class T>
-class A {
-  public:
-    void read(const QJsonObject &json);
-    void write(QJsonObject &json) const;
-};
-
+/** Jede Klasse, dessen Objekte in JSON-Format über das Netzwerk verschickt werden,
+ * implementiert die Methoden read(const QJsonObject &json) und write(QJsonObject &json), somit
+ * werden die decode- und encode Methoden der Klasse Communicator uebersichtlicher.
+**/
 class Communicator
 {
 public:
+    /// Dient zur Kennzeichnung der Befehle
+    enum ObjectType
+    {
+        prepareAttack = 0,
+        attackResult = 0,
+        player = 1
+    };
+
     Communicator();
     ~Communicator();
 
+    /// Versendet die Befehle
     Connection connection;
 
-    void sendPrepareAttack();
+    void readData();
+    void sendData();
 
 private:
-    template <class T>
-    void encodeJson(A, QString&);
-    void decodeJson(QString, A&);
-};
+    /// Uebersetzen der Klassen in JSON und andersherum
+    void encodePlayerToJsonString(Player, QString&);
+    void decodeJsonStringToPlayer(QString, Player&);
+    void encodeAttackToJsonString(Player, QString&);
+    void decodeJsonStringToAttack(QString, Player&);
+
+    /// Erstellt ein
+    QJsonObject createJsonObjectFromString(const QString& in);
 
 } // NS MODEL
 #endif // COMMUNICATOR_H
