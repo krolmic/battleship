dd6131dbe8ae79ac2cc3cdf7f6c5c0fa62681215
coffeemaker2017 <info@michael-krol.de>
2017-07-04 22:36:34 +0200

    MODEL::Communicator::recieveData(const QByteArray& data)

--- battleship/model/communicator.cpp
+++ battleship/model/communicator.cpp
@@ -5,25 +5,44 @@
 #include <memory>
 
 MODEL::Communicator::Communicator(   MODEL::Game& game,
-                                                            const std::string& address, 
-                                                            int port,
-                                                            std::function<void()> callbackOnConnected)
-    : game{game}, conn{ std::make_unique<MODEL::ConnectionGuest>( address, port, 
-        [&](const QByteArray& data) {dataReceived(data); },callbackOnConnected) } //TODO std::bind doesn't work ?
+                                     const std::string& address,
+                                     int port,
+                                     std::function<void()> callbackOnConnected)
+    : game{game}, conn{ std::make_unique<MODEL::ConnectionGuest>( address, port,
+                                                                  [&](const QByteArray& data) {recieveData(data); },callbackOnConnected) } //TODO std::bind doesn't work ?
 {
     //std::bind(&MODEL::Communicator::dataReceived, this)
 }
 
 MODEL::Communicator::Communicator(   MODEL::Game& game,
-                                                            std::function<void()> callbackOnConnected)
-    : game{game}, conn{ std::make_unique<MODEL::ConnectionHost>( 
-        [&](const QByteArray& data) {dataReceived(data); },callbackOnConnected) } //TODO std::bind doesn't work ?
+                                     std::function<void()> callbackOnConnected)
+    : game{game}, conn{ std::make_unique<MODEL::ConnectionHost>(
+                            [&](const QByteArray& data) {recieveData(data); },callbackOnConnected) } //TODO std::bind doesn't work ?
 {
     //std::bind(&MODEL::Communicator::dataReceived, this)
 }
 
-
-void MODEL::Communicator::dataReceived(const QByteArray& data)
+void MODEL::Communicator::recieveData(const QByteArray& data)
 {
-    qDebug() << "COMM::dataReceived: " << data;
+    QJsonDocument doc = QJsonDocument::fromJson(data);
+    QJsonObject jsonObject = doc.object();
+    objectType type = static_cast<objectType>(jsonObject["type"].toInt());
+    QJsonObject object = jsonObject["object"].toObject();
+
+    switch(type)
+    {
+    case objectType::result:
+        // use jsonarray and create objects of attackResult
+        break;
+    case objectType::attack:
+        // same
+        break;
+    case objectType::player:
+        // TODO: change constructor in Player class
+        // there is not need for QString pointer
+        Player p(object["name"].toString(),object["PropertyName"].toInt());
+        break;
+    case objectType::timeout:
+        break;
+    }
 }
--- battleship/model/communicator.h
+++ battleship/model/communicator.h
@@ -2,40 +2,51 @@
 #define COMMUNICATOR_H
 
 #include <memory>
+#include <QJsonArray>
+#include <QJsonObject>
+#include <QJsonDocument>
 #include "connection.h"
+#include "gui/player.h"
 
 namespace MODEL {
 
 class Game;
-    
+
 class Communicator
 {
 public:
-//     explicit Communicator(std::unique_ptr<Connection> conn);
+    //     explicit Communicator(std::unique_ptr<Connection> conn);
     /**
      * creates commincator as guest
-     * 
+     *
      * @param callbackOnConnected called when connection is established
      */
     explicit Communicator( MODEL::Game& game,
-                                        const std::string& address, 
-                                        int port,
-                                        std::function<void()> callbackOnConnected);
+                           const std::string& address,
+                           int port,
+                           std::function<void()> callbackOnConnected);
     /**
      * creates commincator as host
-     * 
+     *
      * @param callbackOnConnected called when connection is established
      */
     explicit Communicator( MODEL::Game& game,
-                                        std::function<void()> callbackOnConnected);
+                           std::function<void()> callbackOnConnected);
     
     Communicator(Communicator const &) = delete; //disable copy-constructor
     Communicator& operator=(Communicator const &other) = delete; //disable assign-operator
     Communicator(Communicator&& other) = delete; //disable move-constructor
     Communicator& operator=(Communicator&& other) = delete; //disable move assign-operator
     
+    enum objectType
+    {
+        result,
+        attack,
+        player,
+        timeout
+    };
 private:
-    void dataReceived(const QByteArray& data);
+    void recieveData(const QByteArray& data);
     
     
     Game& game;
