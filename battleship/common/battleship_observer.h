3c04e3df0eff76a3cd3be014040fd960a415aa24
Michael Krol <info@michael-krol.de>
2017-07-06 01:53:11 +0200

    Update battleship_observer.h

--- battleship/common/battleship_observer.h
+++ battleship/common/battleship_observer.h
@@ -3,9 +3,7 @@
 
 class UserInfo;
 
-/**
- * A base interface class, includes methods, which communicate with model and gui
- */
+/// A base interface class, includes methods, which communicate with model and gui
 class BattleshipObserver //https://stackoverflow.com/questions/14323595/best-way-to-declare-an-interface-in-c11
 {
 public:
